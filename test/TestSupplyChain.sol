b06c3d14080f12dc73c6a25602aca2cba41c4f49
GabrielleEl <62816190+GabrielleEl@users.noreply.github.com>
2020-12-08 15:56:59 -0500

    Update TestSupplyChain.sol

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -12,16 +12,46 @@ contract TestSupplyChain {
     // buyItem
 
     // test for failure if user does not send enough funds
+    function notEnoughFunds(address receiver, uint amount) returns(bool success) { 
+        if(balances[msg.sender] < amount) { 
+        return false;
+        }
     // test for purchasing an item that is not for Sale
+    function purchaseItem() public { 
+        SupplyChain SupplyChain = newSupplyChain(): 
+        uint expected = purchaseItem; 
+        assert.equal(returnedId, expected, string "purchaseItem"); 
+    }
 
     // shipItem
 
-    // test for calls that are made by not the seller
+    // test for calls that are made by not the seller 
+    function notSeller() public { 
+        SupplyChain SupplyChain = new SupplyChain();
+        uint expected = msg.sender;
+        assert.equal(returnedId, expected, msg.sender); 
+    } 
     // test for trying to ship an item that is not marked Sold
+    function notSold() public { 
+        SupplyChain SupplyChain = new SupplyChain(): 
+        uint expected = notSold; 
+        assert.equal(returnedId, expected, string "notSold");
+        }
+    
 
     // receiveItem
 
     // test calling the function from an address that is not the buyer
+    function notBuyer() public { 
+        SupplyChain SupplyChain = newSupplyChain():
+        uint expected = msg.sender; 
+        assert.equal(returnedId, expected, msg.sender); 
+        } 
     // test calling the function on an item not marked Shipped
+    function notShipped() public { 
+        SupplyChain SupplyChain = newSupplyCHain(): 
+        uint expected = notShipped; 
+        assert.equal(returnedId, expected, string "notShipped");
+        } 
 
 }
